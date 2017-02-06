6cbeae7a2ce8fe78424032843f2cb36e9da737f2
avanderj <avanderj@indiana.edu>
2017-02-05 19:46:14 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.clock = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(215, 306);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Session length:";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(302, 306);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Short-break length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Long-break length:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "25";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(302, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "5";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(302, 231);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "15";
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.Location = new System.Drawing.Point(232, 342);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(145, 55);
+            this.clock.TabIndex = 9;
+            this.clock.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(287, 416);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Label4";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Your Pomodoro is Finished.";
+            this.notifyIcon1.BalloonTipTitle = "Break Time";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 571);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clock);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pomodoro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hour, min, sec, ms = 0;
+        int sessionLength;
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            clock.Text = "Break Started";
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            sessionLength = Convert.ToInt32(textBox1.Text);
+
+            if (timer1.Enabled && startButton.Text == "Pause")
+            {
+                timer1.Stop();
+                startButton.Text = "Start";
+            }
+            if (timer1.Enabled && startButton.Text == "Start")
+            {
+                startButton.Text = "Pause";
+            }          
+        }
+
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            clock.Text = "00:00";
+            hour = 0;
+            min = 0;
+            sec = 0;
+            ms = 0;
+            startButton.Text = "Start";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clock.Text = hour + ":" + min + ":" + sec + ":" + ms.ToString();
+            ms++;
+            if (ms > 10)
+            {
+                sec++;
+                ms = 0;
+            }
+            if (sec >= 60)
+            {
+                min++;
+                sec = 0;
+            }
+            if (min == sessionLength)
+            {
+                clock.Text = "You're Done!";
+                timer1.Stop();
+                notifyIcon1.BalloonTipTitle = "Pomodoro Finished";
+                notifyIcon1.BalloonTipText = "Start Break";
+                notifyIcon1.ShowBalloonTip(3000);
+            }
+        }
+    }
+}
